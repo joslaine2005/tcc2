9dc310cde7f9cb25a1b69341700f8d4ec027566a
joslaine2005 <143111885+joslaine2005@users.noreply.github.com>
2024-08-16 19:53:58 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulario-joslaine';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//    echo "Erro";
+//}
+//else
+//{
+    //echo "conexão efeituada com sucesso";
+//}
+
+?>
