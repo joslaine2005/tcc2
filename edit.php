bd86a4db4d6eb6f62b7e1875a107a326830f0502
joslaine2005 <143111885+joslaine2005@users.noreply.github.com>
2024-08-26 16:26:28 -0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -16,8 +16,8 @@
         while($user_data = mysqli_fetch_assoc($result))
         {
             $nome = $user_data['nome'];
-            $email = $user_data['email'];
             $senha = $user_data['senha'];
+            $email = $user_data['email'];
             $telefone = $user_data['telefone'];
             $sexo = $user_data['sexo'];
             $data_nasc = $user_data['data_nasc'];
@@ -52,58 +52,55 @@
                 <legend><b>fórmulario de cliente</b></legend>
                 <br></br>   
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" value = "<?php echo $nome ?>" required>
-                    <label for="nome" class="labelInput">nome completo</label>
+                    <input type="text" name="nome" id="nome" class="inputUser" value=<?php echo $nome;?> required>
+                    <label for="nome" class="labelInput">Nome completo</label>
                 </div>
-                <br></br>
+                <br><br>
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" value = "<?php echo $email ?>" required>
-                    <label for="email"class="labelInput">Email</label>
-                </div>
-                <br></br>
-                <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" value = "<?php echo $senha ?>" required>
+                    <input type="text" name="senha" id="senha" class="inputUser" value=<?php echo $senha;?> required>
                     <label for="senha" class="labelInput">Senha</label>
                 </div>
-                <br></br>
+                <br><br>
                 <div class="inputBox">
-                    <input type="tel" name="telefone" id="telefone" class="inputUser" value = "<?php echo $telefone ?>"required>
-                    <label for="telefone"class="labelInput">Telefone</label>
+                    <input type="text" name="email" id="email" class="inputUser" value=<?php echo $email;?> required>
+                    <label for="email" class="labelInput">Email</label>
                 </div>
-                <p>sexo:</p>
-                <input type="radio" id="feminino" name="genero" value="feminino" <?php echo ($sexo =='feminino') ?'checked' : '' ?> required>
+                <br><br>
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" value=<?php echo $telefone;?> required>
+                    <label for="telefone" class="labelInput">Telefone</label>
+                </div>
+                <p>Sexo:</p>
+                <input type="radio" id="feminino" name="sexo" value="feminino" <?php echo ($sexo == 'feminino') ? 'checked' : '';?> required>
                 <label for="feminino">Feminino</label>
                 <br>
-                <input type="radio" id="masculino" name="genero" value="masculino"<?php echo ($sexo =='masculino') ?'checked' : '' ?> required>
+                <input type="radio" id="masculino" name="sexo" value="masculino" <?php echo ($sexo == 'masculino') ? 'checked' : '';?> required>
                 <label for="masculino">Masculino</label>
                 <br>
-                <input type="radio" id="outro" name="genero" value="outro"<?php echo ($sexo =='outro') ?'checked' : '' ?>required>
+                <input type="radio" id="outro" name="sexo" value="outro" <?php echo ($sexo == 'outro') ? 'checked' : '';?> required>
                 <label for="outro">Outro</label>
-                <br></br>
+                <br><br>
+                <label for="data_nasc"><b>Data de Nascimento:</b></label>
+                <input type="date" name="data_nasc" id="data_nasc" value=<?php echo $data_nasc;?> required>
+                <br><br><br>
                 <div class="inputBox">
-                 <label for="data_nascimento"><b>Data de Nascimento:</b></label>
-                <input type="date" name="data_nascimento" id="data_nascimento"value="<?php echo $data_nasc ?>"required>
+                    <input type="text" name="cidade" id="cidade" class="inputUser" value=<?php echo $cidade;?> required>
+                    <label for="cidade" class="labelInput">Cidade</label>
                 </div>
-                <br></br>
+                <br><br>
                 <div class="inputBox">
-                    <input type="text" name="cidade" id="cidade" class="inputUser"value = "<?php echo $cidade ?>" required>
-                <label for="cidade"class="labelInput">Cidade</label>
-                    </div>
-                    <br></br>
-                    <div class="inputBox">
-                        <input type="text" name="estado" id="estado" class="inputUser"value = "<?php echo $estado ?>"required>
-                        <label for="estado"class="labelInput">Estado</label>
-                    </div>
-                    <br><br><br>
-                    <div class="inputBox">
-                        <input type="text" name="endereco" id="endereco" class="inputUser"value = "<?php echo $endereco ?>"required>
-                        <label for="endereco"class="labelInput">Endereço</label>
-                    </div>
-                    <br></br>
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                  <input type="submit" name="update" id="update">
+                    <input type="text" name="estado" id="estado" class="inputUser" value=<?php echo $estado;?> required>
+                    <label for="estado" class="labelInput">Estado</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="endereco" id="endereco" class="inputUser" value=<?php echo $endereco;?> required>
+                    <label for="endereco" class="labelInput">Endereço</label>
+                </div>
+                <br><br>
+    <input type="hidden" name="id" value=<?php echo $id;?>>
+                <input type="submit" name="update" id="update">
             </fieldset>
         </form>
     </div>
 </body>
-</html>
